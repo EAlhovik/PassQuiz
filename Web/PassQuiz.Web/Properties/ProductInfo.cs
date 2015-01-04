a41dd859b3a782e5193fc7e8d47bd45c6036a366
e.alhovik <e.alhovik@gmail.com>
2015-01-04 11:59:33 +0300

    initial project

---
+++ Web/PassQuiz.Web/Properties/ProductInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PassQuiz.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
