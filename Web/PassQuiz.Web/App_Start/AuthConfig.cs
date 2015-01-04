cab2932e526f90e19e770813c1964e53dcfcc57c
e.alhovik <e.alhovik@gmail.com>
2015-01-04 14:06:27 +0300

    set up project with data layer

--- Web/PassQuiz.Web/App_Start/AuthConfig.cs
+++ Web/PassQuiz.Web/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using PassQuiz.Web.Models;
-
-namespace PassQuiz.Web
+﻿namespace PassQuiz.Web
 {
     public static class AuthConfig
     {
